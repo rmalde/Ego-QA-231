7a7e0be3b482511dcdec96077161d8989005983d
Arjun Karanam <akaranam@stanford.edu>
2023-05-14 14:58:57 -0700

    Edited requirements

--- baseline.py
+++ baseline.py
@@ -1,4 +1,6 @@
 import random
+import langchain
+import os
 
 '''This approach simple takes the middle frame from each questions, 
 uses CLIP to classify that immage, and passes that result plus the quesiton into an LLM
@@ -19,15 +21,16 @@ class Baseline_Test():
         # Return the questions, frames, answers, and answer choices at those indices
         return self.questions[indices], self.frames[indices], self.answers[indices], self.answer_choices[indices]
     
-    def extract_frame():
+    def extract_frame(frame_list):
         #Extract the frame given the question and the id 
-        pass
+        return None
 
-    def classify_frame():
-        #Pass frame into CLIP and retrieve result 
-        pass
+    def classify_frame(frame):
+        #Pass frame into CLIP and retrieve result
+        if frame == None:
+            return "dummy result"
 
-    def answer_question():
+    def answer_question(question, answer_choices, clip_result):
         #Pass question and CLIP result to GPT and retrieve answer
         pass
 
@@ -35,6 +38,11 @@ class Baseline_Test():
         # Evaluate the answer compared to the actual answer
         pass
 
-    def run_baseline():
+    def run_baseline(self):
         #Run the baseline model
-        pass
+        sampled_questions, sampled_frames, sampled_answers, sampled_answer_choices = self.sample()
+        for i in range(len(sampled_questions)):
+            frame = self.extract_frame(sampled_frames[i])
+            clip_result = self.classify_frame(frame)
+            chosen_answer = self.answer_question(sampled_questions[i], sampled_answer_choices[i], clip_result)
+            self.evaluate_answer(chosen_answer, sampled_answers[i])
